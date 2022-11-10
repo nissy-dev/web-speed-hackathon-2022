827c8d885db11484e2ef9f6e4788df6008da7e46
nissy-dev <nd.12021218@gmail.com>
2022-11-11 01:16:25 +0900

    archive

--- src/client/foundation/routes.jsx
+++ src/client/foundation/routes.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Route, Routes as RouterRoutes } from "react-router-dom";
 
 import { CommonLayout } from "./layouts/CommonLayout";
+import Top from "./pages/Top";
+import Odds from "./pages/races/Odds";
+import RaceCard from "./pages/races/RaceCard";
+import RaceResult from "./pages/races/RaceResult";
 
-const Top = React.lazy(() => import("./pages/Top"));
-const Odds = React.lazy(() => import("./pages/races/Odds"));
-const RaceCard = React.lazy(() => import("./pages/races/RaceCard"));
-const RaceResult = React.lazy(() => import("./pages/races/RaceResult"));
+// const Top = React.lazy(() => import("./pages/Top"));
+// const Odds = React.lazy(() => import("./pages/races/Odds"));
+// const RaceCard = React.lazy(() => import("./pages/races/RaceCard"));
+// const RaceResult = React.lazy(() => import("./pages/races/RaceResult"));
 
 /** @type {React.VFC} */
 export const Routes = () => {
@@ -16,41 +20,41 @@ export const Routes = () => {
         <Route
           index
           element={
-            <React.Suspense fallback={null}>
-              <Top />
-            </React.Suspense>
+            // <React.Suspense fallback={null}>
+            <Top />
+            // </React.Suspense>
           }
         />
         <Route
           element={
-            <React.Suspense fallback={null}>
-              <Top />
-            </React.Suspense>
+            // <React.Suspense fallback={null}>
+            <Top />
+            // </React.Suspense>
           }
           path=":date"
         />
         <Route path="races/:raceId">
           <Route
             element={
-              <React.Suspense fallback={null}>
-                <RaceCard />
-              </React.Suspense>
+              // <React.Suspense fallback={null}>
+              <RaceCard />
+              // </React.Suspense>
             }
             path="race-card"
           />
           <Route
             element={
-              <React.Suspense fallback={null}>
-                <Odds />
-              </React.Suspense>
+              // <React.Suspense fallback={null}>
+              <Odds />
+              // </React.Suspense>
             }
             path="odds"
           />
           <Route
             element={
-              <React.Suspense fallback={null}>
-                <RaceResult />
-              </React.Suspense>
+              // <React.Suspense fallback={null}>
+              <RaceResult />
+              // </React.Suspense>
             }
             path="result"
           />
